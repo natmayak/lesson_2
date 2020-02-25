2cfaf450faac00b74654b3aed6ebb0ff2f2a05fa
Nat Nikulina <natalie.nikulina@gmail.com>
2020-02-25 20:50:06 +0300

    ephen module v.2 added

--- modules_ephem_hw_v.2.py
+++ modules_ephem_hw_v.2.py
@@ -4,6 +4,8 @@ import ephem
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
          'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
+galaxy = {'Mars': ephem.Mars, 'Venus': ephem.Venus, 'Moon': ephem.Moon, 'Jupiter': ephem.Jupiter, 'Saturn': ephem.Saturn}
+
 
 def main():
     mybot = Updater("1097577104:AAHoqylJFKEK7HDsanQNHTYW5qtSM-2rjqo", request_kwargs=PROXY)
@@ -31,19 +33,26 @@ def send_planet(bot, update):
     user_text = update.message.text
     planet_text = user_text.split(' ')
     planet = planet_text[1]
-    print(planet)
-    update.message.reply_text(planet)
-    # import datetime
-    # today = datetime.datetime.today()
-    # print(today.strftime("%m/%d/%Y"))
-    # planet_ephem = getattr(ephem, planet)(today.strftime("%Y/%m/%d"))
-    date = ephem.now()
-    print(date)
-    update.message.reply_text(date)
-    planet_ephem = getattr(ephem, planet)(date)
-    constellation = ephem.constellation(planet_ephem)
-    print(constellation)
-    update.message.reply_text(constellation)
+
+    if galaxy.get(planet) is not None:
+        print(planet)
+        update.message.reply_text(planet)
+        # import datetime
+        # today = datetime.datetime.today()
+        # print(today.strftime("%m/%d/%Y"))
+        # planet_ephem = getattr(ephem, planet)(today.strftime("%Y/%m/%d"))
+        date = ephem.now()
+        print(date)
+        update.message.reply_text(date)
+        planet_ephem = galaxy[planet](date)
+        constellation = ephem.constellation(planet_ephem)
+        print(constellation)
+        update.message.reply_text(constellation)
+    else:
+        update.message.reply_text(planet)
+        no_planet_text = 'There is no such planet in our database'
+        print(no_planet_text)
+        update.message.reply_text(no_planet_text)
 
 
 main()
