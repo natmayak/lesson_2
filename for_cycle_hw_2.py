9358cfe665ea9ebed931198f68c33173096d2b71
Nat Nikulina <natalie.nikulina@gmail.com>
2020-02-28 09:59:34 +0300

    updated 27.02

--- for_cycle_hw_2.py
+++ for_cycle_hw_2.py
@@ -1,15 +1,17 @@
-school_scores = [{'school_class': '4a', 'scores': [3,4,4,5,2]}, {'school_class': '7b', 'scores': [5,4,3,5,5]}, {'school_class': '8c', 'scores': [2,3,4,5,5]}, {'school_class': '11a', 'scores': [5,5,3,5,2]}, {'school_class': '9d', 'scores': [3,4,4,4,3]}]
-amount_of_classes = len(school_scores)
-scores_sum = 0
+school_scores = [{'school_class': '4a', 'scores': [3,4,4,4,5,2]}, {'school_class': '7b', 'scores': [5,4,3,5]}, {'school_class': '8c', 'scores': [2,3,4,5,5]}, {'school_class': '11a', 'scores': [5,5,3,5,2]}, {'school_class': '9d', 'scores': [3,4,4,4,3]}]
+class_scores_sum = 0
+school_scores_sum = 0
+class_scores_amount = 0
+school_scores_amount = 0
 
-# for class_scores in school_scores:
-#     av_school_score = sum(class_scores[:(amount_of_classes - 1)])
-#I didn't guess how to count school average prior to class average.  Still thinking
-
-for score in school_scores[amount_of_classes - 1]["scores"]:
-    scores_sum = sum(school_scores[amount_of_classes - 1]["scores"])
-    scores_avg = scores_sum / len(school_scores[amount_of_classes - 1]["scores"])
-    class_name = school_scores[amount_of_classes - 1]['school_class']
-    print(f"GPA per {class_name} {scores_avg}, total sum: {scores_sum}")
-    amount_of_classes = amount_of_classes - 1
-
+for class_scores in school_scores:
+    school_scores_sum += sum(class_scores['scores'])
+    school_scores_amount += len(class_scores['scores'])
+    class_scores_sum = sum(class_scores['scores'])
+    class_scores_amount = len(class_scores['scores'])
+    scores_avg = class_scores_sum / class_scores_amount
+    class_name = class_scores['school_class']
+    print(f"GPA per {class_name} {scores_avg}, total sum: {class_scores_sum}")
+school_result = str(school_scores_sum)
+school_average = str (school_scores_sum/school_scores_amount)
+print(f'Total GPA per school is {school_average}, total sum being {school_result}')
---
+++ for_cycle_hw_2_(bad).py
@@ -0,0 +1,12 @@
+school_scores = [{'school_class': '4a', 'scores': [3,4,4,5,2]}, {'school_class': '7b', 'scores': [5,4,3,5,5]}, {'school_class': '8c', 'scores': [2,3,4,5,5]}, {'school_class': '11a', 'scores': [5,5,3,5,2]}, {'school_class': '9d', 'scores': [3,4,4,4,3]}]
+amount_of_classes = len(school_scores)
+scores_sum = 0
+
+#Weird longer version counting GPA per class from the end of the list, but anyway:
+
+for score in school_scores[amount_of_classes - 1]["scores"]:
+    scores_sum = sum(school_scores[amount_of_classes - 1]["scores"])
+    scores_avg = scores_sum / len(school_scores[amount_of_classes - 1]["scores"])
+    class_name = school_scores[amount_of_classes - 1]['school_class']
+    print(f"GPA per {class_name} {scores_avg}, total sum: {scores_sum}")
+    amount_of_classes = amount_of_classes - 1
